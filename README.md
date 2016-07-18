882221627d7e1f0066729ae1be2f7b8870112bda
jurin-n <jurin.nakano@gmail.com>
2016-07-18 11:35:36 +0900

    add Publish/Subscribe Pattern.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # rabbitMQ-prototype
 
 rabbitMQ試したのでメモ。Mac OS/Xで試しました。
+チュートリアル(https://www.rabbitmq.com/getstarted.html)を試してみました。
 
 ### セットアップ
 #### サーバインストール
@@ -17,14 +18,15 @@ mvn clean package
 
 #### 受信プログラム起動
 java -cp .:target/dependency/amqp-client-3.6.3.jar:target/rabbitMQ-prototype.jar com.example.Recv
+java -cp .:target/dependency/amqp-client-3.6.3.jar:target/rabbitMQ-prototype.jar com.example.ReceiveLogs
 
 #### メッセージ送信
 java -cp .:target/dependency/amqp-client-3.6.3.jar:target/rabbitMQ-prototype.jar com.example.Send
-
+java -cp .:target/dependency/amqp-client-3.6.3.jar:target/rabbitMQ-prototype.jar com.example.EmitLog "Sent Message One."
 
 ### 参考
 #### キュー確認
 /usr/local/sbin/rabbitmqctl list_queues
-
+sudo /usr/local/sbin/rabbitmqctl list_queues name messages_ready messages_unacknowledged
 
 
---
+++ src/main/java/com/example/EmitLog.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLog {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] argv)
+                  throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+        String message = getMessage(argv);
+
+        channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+    //...
+
+    private static String getMessage(String[] argv) {
+        // TODO Auto-generated method stub
+        return argv[0];
+    }
+}
